f57dd7a5537bac0974ca2f217acc4b2167622c29
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-10-04 18:08:20 +0300

    finish task

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello Node.js world");
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      console.log(contactsList);
+      break;
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+const argv = program.opts();
+invokeAction(argv);
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "5" });
+// invokeAction({
+//   action: "add",
+//   name: "Mango",
+//   email: "mango@gmail.com",
+//   phone: "322-22-22",
+// });
+// invokeAction({ action: "remove", id: "3" });
